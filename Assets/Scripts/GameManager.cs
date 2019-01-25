1821088739800b9d96fbf6093308d030265533e8
esfox <osairis100@gmail.com>
2019-01-25 23:12:22 +0800

    Scripts

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+
+    private static GameManager instance;
+    public GameManager Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+    public void ObjectPickup()
+    {
+        
+    }
+}
