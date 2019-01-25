b80ca574b7b7191bc84e60609461a96e3a073594
TheRayOfSeasons <sison.ray@ciit.edu.ph>
2019-01-25 23:09:13 +0800

    AudioManager

---
+++ GameJam/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour 
+{
+    // Create a separate GameObject in the scene with this script.
+
+    private static AudioManager instance = null;
+    public static AudioManager Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake ()
+    {
+        instance = this;
+    }
+
+    //Fill these in the inspector. 
+    [SerializeField] private AudioSource[] calmAudio;
+    [SerializeField] private AudioSource[] desperationAudio;
+    [SerializeField] private AudioSource[] hopeAudio;
+    [SerializeField] private AudioSource[] panicAudio;
+
+    //Stores Currently Playing BGM
+    private AudioSource currentlyPlaying = null;
+ 
+    public enum AudioType
+    {
+        Calm,
+        Desperation,  
+        Hope,
+        Panic
+    };
+
+    //Takes an AudioType as a parameter
+    //Example: PlayAudio(AudioManager.AudioType.Calm);
+    public void PlayAudio (AudioType audioType)
+    {
+        switch (audioType)
+        {
+            case AudioType.Calm:
+                playRandom(calmAudio);
+                print("Wow Audio");
+                break;
+            case AudioType.Desperation:
+                playRandom(desperationAudio);
+                break;
+            case AudioType.Hope:
+                playRandom(hopeAudio);
+                break;
+            case AudioType.Panic:
+                playRandom(panicAudio);
+                break;
+        }
+    }
+
+    //Stops Audio
+    public void StopAudio ()
+    {
+        currentlyPlaying.Stop();
+    }
+
+    //plays a random track from an audio array
+    private void playRandom (AudioSource[] audioArray)
+    {
+        currentlyPlaying = audioArray[Random.Range(0, audioArray.length - 1)];
+        currentlyPlaying.Play();
+    }
+}
