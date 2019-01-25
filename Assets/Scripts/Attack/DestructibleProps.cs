de953b1d21540a8186208f34423672d2d2cc9919
GolRouge <gol.rouge@gmail.com>
2019-01-26 02:11:46 +0800

    Props Damage. plus camera animation

---
+++ Assets/Scripts/Attack/DestructibleProps.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleProps : MonoBehaviour
+{
+    public int health;
+   
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void TakeDamage (int damage){
+
+        health -= damage;
+        Debug.Log("Hit");
+    }
+}
---
+++ Assets/Scripts/Attack/PlayerAttack.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+   private float AttackDelay;
+   public float StartAttackDelay;
+
+   public Transform AttackerPos;
+   public float damageRange;
+   public LayerMask destructibles;
+   public int damage;
+   public Animator camAnim;
+
+    void Update()
+    {
+        if (AttackDelay <= 0)
+        {
+            //if (Input.GetMouseButton(0))
+            if (Input.GetKey(KeyCode.Space))
+            {
+                camAnim.SetTrigger("Shake");
+                Collider2D[] propsToDamage = Physics2D.OverlapCircleAll(AttackerPos.position, damageRange, destructibles);
+                for (int i = 0; i < propsToDamage.Length; i++)
+                {
+                    
+                    propsToDamage[i].GetComponent<DestructibleProps>().TakeDamage(damage);
+                }
+
+                Debug.Log("ATTACK");
+                AttackDelay = StartAttackDelay;
+            }
+            
+        }
+
+        else
+        {
+            AttackDelay -= Time.deltaTime;
+            //Debug.Log(AttackDelay);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(AttackerPos.position, damageRange);
+    }
+}
