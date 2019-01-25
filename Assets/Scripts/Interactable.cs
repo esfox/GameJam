b53a552df37da11865999b3a9faec6bf812b7c39
esfox <osairis100@gmail.com>
2019-01-25 23:47:47 +0800

    A good update

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class Interactable : MonoBehaviour
 {
+    [SerializeField] private int index;
+    public int Index
+    {
+        get { return index; }
+    }
+
     void Start()
     {
         GetComponent<Collider2D>().isTrigger = true;
